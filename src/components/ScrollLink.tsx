4d6aa0f8df87de8c484f60108ab0b15f49318abb
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2026-06-05 00:45:37 +0500

    feat: scroll to top link added

---
+++ src/components/ScrollLink.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import type { ComponentProps, MouseEvent } from "react";
+
+export type ScrollLinkProps = Readonly<ComponentProps<typeof Link>>;
+
+function resolveHref(href: ScrollLinkProps["href"]): string {
+  if (typeof href === "string") {
+    return href;
+  }
+  if (href && typeof href === "object" && "pathname" in href && href.pathname) {
+    return href.pathname;
+  }
+  return "";
+}
+
+export function ScrollLink(props: Readonly<ScrollLinkProps>) {
+  const { href, onClick, ...rest } = props;
+  const pathname = usePathname();
+
+  const handleClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    if (pathname === resolveHref(href)) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+    onClick?.(e);
+  };
+
+  return <Link href={href} onClick={handleClick} {...rest} />;
+}
